c6cffa3467764ca8dc1327d8a0741e34f2ba93f4
broccoli16 <97226207+broccoli16@users.noreply.github.com>
2022-01-10 14:51:06 +0900

    2022.1.10 jsの勉強

--- src/index.js
+++ src/index.js
@@ -1,10 +1,64 @@
-import "./styles.css";
+// let name = "kaito";
+// console.log(name);
+// name = "taishi";
+// console.log(name);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// const name = "kaito";
+// console.log(name);
+// name="taishi";
+
+// let a=2;
+// const func1 = (kazu) => {
+//   a += 3;
+//   kazu=a;
+//   return kazu;
+// };
+// console.log(func1());
+
+// const chara = {
+//   name : "taishi",
+//   age : 21
+// };
+
+// const {name , age} = chara;
+// console.log(name);
+// const name = "はにゃ";
+// const age = 2;
+
+// const message = `私の名前は${name}です。${age}歳です`;
+
+// console.log(message);
+
+// let arr1 = [1, 2, 3, 4];
+// arr1 = [0,...arr1,5];
+// const [num1,num2,...arr2] = arr1;
+// console.log(arr1);
+// console.log(num1);
+// console.log(num2);
+// console.log(arr2);
+
+// console.log(...arr1);
+// const sum = (num1,num2) => console.log(num1+num2);
+//sum(arr1[2],arr1[5]);
+// sum(...arr1);
+
+//const array = ["はにゃ", "げろ", "ぺろ", "はにゃ"];
+// // for(let i=0;i<array.length;i++){
+// //   console.log(array[i]);
+// // }
+//array.map((name) => console.log(name));
+//  const newarray = array.map((name) => {
+//    return name;
+//  });
+//  console.log(newarray);
+
+// const array_hanya = array.filter((name) => {
+//   return name==="はにゃ";
+// });
+// console.log(array_hanya);
+
+// const trueHanya = array.map((name) => {
+//   return name === "はにゃ" ? `${name}?` : name;
+// });
+// console.log(trueHanya);
+//三項演算子 条件 ? 真の処理 : 偽の処理
